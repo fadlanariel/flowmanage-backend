acdc54d88e9ca0ec53b3059286dd2d8abdbec5a6
Fadlan Ariel <fadlan.ariel@ui.ac.id>
2026-01-01 14:05:30 +0700

    chore: initial spring boot project, database, & migration

---
+++ src/test/java/com/flowmanage/FlowmanageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flowmanage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlowmanageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
